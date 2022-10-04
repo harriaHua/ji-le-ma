7d17b5ccedbdb928d3a11e9a4f0b675530ffcff6
YIIIIj <114039143+YIIIIj@users.noreply.github.com>
2022-10-04 16:49:37 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,30 +2,22 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
-import uView from '@/uni_modules/uview-ui'
-import request from '@/api/request.js'
-import store from "./store";
-Vue.prototype.$request = request
-Vue.prototype.$store = store
-
-Vue.use(uView)
+import uView from '@/uni_modules/uview-ui';  
+Vue.use(uView);
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-	...App,
-	store,
+    ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import {
-	createSSRApp
-} from 'vue'
+import { createSSRApp } from 'vue'
 export function createApp() {
-	const app = createSSRApp(App)
-	return {
-		app
-	}
+  const app = createSSRApp(App)
+  return {
+    app
+  }
 }
 // #endif
