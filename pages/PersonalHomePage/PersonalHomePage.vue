b6c6ab8851a66bd063faee3e496febb0bb6e8fa6
dixuanliu <dixuanliu@icloud.com>
2022-10-08 16:45:09 +0800

    跳转处理

--- pages/PersonalHomePage/PersonalHomePage.vue
+++ pages/PersonalHomePage/PersonalHomePage.vue
@@ -6,36 +6,55 @@
 				<view class="user-info-change">
 					<u-text :text="$store.state.userId"></u-text>
 					<u-button
-						customStyle="	width: 140rpx;height: 52rpx;font-size: 28rpx;padding: 10rpx 10rpx;border-radius: 40rpx;">
+						customStyle="width: 140rpx;height: 52rpx;font-size: 28rpx;padding: 10rpx 10rpx;border-radius: 40rpx;">
 						修改信息
 					</u-button>
 				</view>
 				<view class="send-package">
 					<view class="send-package-button">
 						<homepagebutton iconname="car" iconcolor="white" buttoncolor="red" size="74rpx" text="去寄快递"
-							:customStyle="styleone">
+							:customStyle="customstyle.styleone">
 						</homepagebutton>
 					</view>
 				</view>
 			</view>
 			<view class="nav-bar">
+				<homepagebutton :buttoncolor="homebutton.buttoncolor" :iconname="homebutton.iconname"
+					:iconcolor="homebutton.iconcolor" @jump="jump(homebutton.url,homebutton.way)"
+					:text="homebutton.text" :customStyle="customstyle.styletwo">
+				</homepagebutton>
 				<view v-for="item in navbuttontype">
 					<homepagebutton :buttoncolor="item.buttoncolor" :iconname="item.iconname"
-						:iconcolor="item.iconcolor" @jump="jump(item.url)" :text="item.text" :customStyle="styletwo">
+						:iconcolor="item.iconcolor" @jump="jump(item.url,item.way)" :text="item.text"
+						:customStyle="customstyle.styletwo">
 					</homepagebutton>
 				</view>
 			</view>
 			<view class="tools-bar">
-				<text style="font: 44rpx bold;margin: 14rpx 36rpx;" class="tools-bar-text">基础功能</text>
+				<view class="tools-bar-text">
+					<u--text size="44rpx" bold text="基础功能">
+					</u--text>
+				</view>
 				<view class="function-buttons" v-for="item in functionbutton">
 					<homepagebutton :buttoncolor="item.buttoncolor" :iconname="item.iconname"
-						:iconcolor="item.iconcolor" @jump="jump(item.url)" :text="item.text" :customStyle="styletwo">
+						:iconcolor="item.iconcolor" @jump="jump(item.url,item.way)" :text="item.text"
+						:customStyle="customstyle.styletwo">
 					</homepagebutton>
 
 				</view>
 			</view>
 			<view class="more-func">
-
+				<view class="more-func-text">
+					<u--text size="44rpx" bold text="其他功能"></u--text>
+				</view>
+				<view class="swiper">
+					<u-swiper :list="swiperList" autoplay="true" indicator circular="true" radius="40rpx"></u-swiper>
+				</view>
+				<view class="more-func-button">
+					<view v-for="item in customstyle.stylethree">
+						<u-button :customStyle="item.style" :text="item.text"></u-button>
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -47,21 +66,48 @@
 	export default {
 		data() {
 			return {
-				styletwo: "width: 106rpx;height:106rpx;border-radius:20px",
-				styleone: "width: 106rpx;height:106rpx;border-radius:100%",
+				swiperList: [
+					'https://cdn.uviewui.com/uview/swiper/swiper1.png',
+					'https://cdn.uviewui.com/uview/swiper/swiper2.png',
+					'https://cdn.uviewui.com/uview/swiper/swiper3.png',
+				],
+				customstyle: {
+					"styletwo": "width: 106rpx;height:106rpx;border-radius:20px",
+					"styleone": "width: 106rpx;height:106rpx;border-radius:100%",
+					"stylethree": [{
+							style: "color:white;width:100%;height:100%;border-radius:20px;background:url('../../static/buttonbg/food.jpg');-moz-background-size: 100% 100%;background-size: 100% 100%;",
+							text: "餐饮美食"
+						},
+						{
+							style: "color:white;width:100%;height:100%;border-radius:20px;background:url('../../static/buttonbg/hotel.jpg');-moz-background-size: 100% 100%;background-size: 100% 100%;",
+							text: "积分兑换"
+						},
+						{
+							style: "color:white;width:100%;height:100%;border-radius:20px;background:url('../../static/buttonbg/enterta.jpg');-moz-background-size: 100% 100%;background-size: 100% 100%;",
+							text: "影音娱乐"
+						},
+						{
+							style: "color:white;width:100%;height:100%;border-radius:20px;background:url('../../static/buttonbg/makeup.jpg');-moz-background-size: 100% 100%;background-size: 100% 100%;",
+							text: "美妆优选"
+						},
+					]
+				},
+
+				homebutton: {
+					iconname: "home",
+					text: "首页",
+					buttoncolor: "red",
+					iconcolor: "white",
+					url: "/pages/home/home",
+					way: "relaunch"
+				},
 				navbuttontype: [{
-						iconname: "home",
-						text: "首页",
-						buttoncolor: "red",
-						iconcolor: "white",
-						url: "/pages/home/home",
-					},
-					{
 						iconname: "rmb-circle",
 						text: "余额",
 						buttoncolor: "",
 						iconcolor: "",
-						url: "/pages/recharge/recharge"
+						url: "/pages/recharge/recharge",
+						way: ""
 					},
 					{
 						iconname: "star",
@@ -73,16 +119,17 @@
 					}
 				],
 				functionbutton: [{
-						iconname: "order",
+						iconname: "file-text",
 						text: "我的订单",
 						buttoncolor: "red",
 						iconcolor: "white",
-						url: "",
+						url: "/pages/note/note",
+						way: ""
 					},
 					{
 						iconname: "map",
 						text: "地址管理",
-						action: ""
+						url: "/pages/address/address",
 					},
 					{
 						iconname: "scan",
@@ -116,11 +163,18 @@
 			homepagebutton
 		},
 		methods: {
-			jump(url) {
-				console.log("yes")
-				uni.navigateTo({
-					url: url
-				})
+			jump(url, way) {
+				if (way === "relaunch") {
+					console.log("yes")
+					uni.reLaunch({
+						url: url
+					})
+				} else {
+					console.log("no")
+					uni.navigateTo({
+						url: url
+					})
+				}
 			}
 		},
 	}
@@ -134,13 +188,14 @@
 	.content {
 		display: flex;
 		flex-direction: column;
-		background-color: #ff0000;
+		background-image: url("../../static/homepagebgimage/rect286.png");
 
 		.user-information {
 			height: 200rpx;
 			background-color: #fff;
 			display: flex;
 			justify-content: left;
+			box-shadow: -1rpx 0rpx 8rpx 2rpx #d8d8d8;
 			margin: 16rpx 38rpx;
 			border-radius: 40rpx;
 
@@ -190,7 +245,7 @@
 			margin: 16rpx 38rpx;
 			border-radius: 40rpx;
 			align-items: center;
-
+			box-shadow: -1rpx 0rpx 8rpx 2rpx #d8d8d8;
 		}
 
 		.tools-bar {
@@ -201,22 +256,43 @@
 			grid-template-columns: repeat(4, 1fr);
 			margin: 16rpx 38rpx;
 			border-radius: 40rpx;
+			box-shadow: -1rpx 0rpx 8rpx 2rpx #d8d8d8;
 
 			.tools-bar-text {
 				grid-area: 1/1/2/5;
-				margin: 9rpx 18rpx;
+				margin: 14rpx 36rpx;
 			}
 		}
 
 	}
 
 	.more-func {
-		height: 764rpx;
+		height: 800rpx;
 		background-color: #fff;
-		display: flex;
-		flex-direction: column;
-		flex-wrap: wrap;
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 77rpx repeat(3, 1fr);
 		margin: 16rpx 38rpx;
 		border-radius: 40rpx;
+		box-shadow: -1rpx 0rpx 8rpx 2rpx #d8d8d8;
+
+		.more-func-text {
+			margin: 14rpx 36rpx;
+			grid-area: 1/1/2/4;
+		}
+
+		.swiper {
+			margin: 14rpx 36rpx;
+			grid-area: 2/1/3/4;
+		}
+
+		.more-func-button {
+			height: 420rpx;
+			margin: 14rpx 36rpx;
+			display: grid;
+			grid-template-areas: "a b"
+				"c d";
+			grid-gap: 18rpx;
+		}
 	}
 </style>
