685da725850dfaee10d3b4a0b3b1b66b0a72ff90
BridTree <114039379+BridTree@users.noreply.github.com>
2022-10-08 21:45:18 +0800

    寄快递

--- main.js
+++ main.js
@@ -1,31 +1,22 @@
-import App from "./App";
+import App from './App'
+import './static/iconfont.css'
 
 // #ifndef VUE3
-import Vue from "vue";
-import uView from "@/uni_modules/uview-ui";
-import request from "@/api/request.js";
-import store from "./store";
-Vue.prototype.$request = request;
-Vue.prototype.$store = store;
-
-Vue.use(uView);
-Vue.config.productionTip = false;
-App.mpType = "app";
+import Vue from 'vue'
+Vue.config.productionTip = false
+App.mpType = 'app'
 const app = new Vue({
-	...App,
-	store,
-});
-app.$mount();
+    ...App
+})
+app.$mount()
 // #endif
 
 // #ifdef VUE3
-import {
-	createSSRApp
-} from "vue";
+import { createSSRApp } from 'vue'
 export function createApp() {
-	const app = createSSRApp(App);
-	return {
-		app,
-	};
+  const app = createSSRApp(App)
+  return {
+    app
+  }
 }
 // #endif
