56fde10baf5739e7f2179fcf4886b0fac4dec964
dixuanliu <dixuanliu@icloud.com>
2022-10-08 14:14:46 +0800

    插件

---
+++ pages/components/homepagebutton.vue
@@ -0,0 +1,17 @@
+<template>
+	<view>
+		<u-button :shape="shape" @click="$emit('jump')" :color="buttoncolor" :customStyle="customStyle">
+			<u-icon :name="iconname" :color="iconcolor" size="62rpx"></u-icon>
+		</u-button>
+		<u-text :text="text" size="24rpx" align="center"></u-text>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: ['iconname', 'buttoncolor', 'iconcolor', 'text', 'shape', 'customStyle'],
+	}
+</script>
+
+<style>
+</style>
