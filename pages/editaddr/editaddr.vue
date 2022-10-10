5b90bfe8734722d640dd37e1a07c79fdaf0e8d58
YIIIIj <114039143+YIIIIj@users.noreply.github.com>
2022-10-10 14:18:13 +0800

    Add files via upload

--- pages/editaddr/editaddr.vue
+++ pages/editaddr/editaddr.vue
@@ -1,38 +1,29 @@
 <template>
-	<usertable :olduser="olduser" @submit="submit"></usertable>
+	<usertable :addressId="addressId" @submit="submit"></usertable>
 </template>
 
 <script>
-	import data from "@/src/data.js"
+	// import data from "@/src/data.js"
 	import usertable from "../components/usertable.vue"
 	export default {
 		components: {
 			usertable
 		},
 		onLoad: function(option) {
-			this.olduser = data[option.id]
-			// console.log(olduser);
-			this.num = option.id //返回数据的id，即第几个数据
-			// console.log(num)
+			this.addressId = option.id //返回数据的id，即第几个数据
+			// console.log(this.addressId)
 		},
 		data() {
 			return {
-				data, //总数据
-				olduser: {
-					name: '',
-					number: '',
-					area: '',
-					address: '',
-					code: ''
-				},
-				num:''
+				addressId:''
 			}
 		},
 
 		methods: {
 			async submit(e) {
 				// console.log(e)
-				this.data.splice(this.num, 1, e); //将从子组件里修改过后的数据，替换原本的数据
+				let res = await this.$request.post('/updateAddress', e);
+				// this.data.splice(this.num, 1, e); //将从子组件里修改过后的数据，替换原本的数据
 				uni.navigateBack({
 					url: '/pages/address/address'
 				})
